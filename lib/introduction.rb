f64ca6a9a6fdb9654585485ff61869366eccba03
super-block-0637 <cyrusizadipour@gmail.com>
2020-06-18 01:34:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,10 @@
+def introduction (name)
+  puts "Hi, my name is #{name}."
+end
 
 
 
-
-def introduction_with_languange (name, language)
+def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning the programming language #{language}."
 end
 
